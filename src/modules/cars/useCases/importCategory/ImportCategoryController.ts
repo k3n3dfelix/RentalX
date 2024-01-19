976fab3df10ee2199bd16ef7b1dcbf4c676ce701
Kened Silva Felix <ksfelix1@stefanini.com>
2024-01-18 22:58:03 -0300

    feat: adicionado rota upload de arquivo com categorias

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -0,0 +1,17 @@
+import { Response, Request } from "express";
+
+import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
+
+class ImportCategoryController {
+  constructor(private importCategoryUSeCase: ImportCategoryUseCase) {}
+
+  handle(request: Request, response: Response): Response {
+    const { file } = request;
+
+    this.importCategoryUSeCase.execute(file);
+
+    return response.send();
+  }
+}
+
+export { ImportCategoryController };
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,13 +1,19 @@
 import { Router } from "express";
+import multer from "multer";
+
 // import { v4 as uuidV4 } from "uuid";
 
 // import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
 // import { PostgresCategoriesRepository } from "../modules/cars/repositories/PostgresCategoryRepository";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { importCategoryController } from "../modules/cars/useCases/importCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
 
+const upload = multer({
+  dest: "./tmp",
+});
 // const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
@@ -18,6 +24,10 @@ categoriesRoutes.get("/", (request, response) => {
   // const all = categoriesRepository.list();
 
   return listCategoriesController.handle(request, response);
+});
+
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+  return importCategoryController.handle(request, response);
 });
 
 export { categoriesRoutes };
